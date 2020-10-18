ab44923424628fe80d022262f4eb42413b57e4f9
unknown <adhiragathan12@gmail.com>
2020-10-18 22:18:33 +0700

    initial commit

---
+++ src/if1/pkg10119025/latihan10/logika/IF110119025Latihan10Logika.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119025.latihan10.logika;
+
+/**
+ *
+ * @author Adhira Fahri Gathan
+ */
+public class IF110119025Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int umur = 19;
+       boolean dewasa;
+       dewasa = (umur >= 17);
+       System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
